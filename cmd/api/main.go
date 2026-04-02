db5f0e1f71f95fd01a43db06ba717fd479f13ecb
Kevin Wiratama <kevinhia063@gmail.com>
2026-04-02 18:13:54 +0700

    Update lagi

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,29 +6,27 @@ import (
 	"si-baper-backend/config"
 	"si-baper-backend/routes"
 	"si-baper-backend/seeders"
-	"si-baper-backend/utils"
-
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
 
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Println("Peringatan: File .env tidak ditemukan")
+		log.Println("Peringatan: File .env tidak ditemukan, menggunakan environment dari sistem cloud.")
 	}
 
 	config.ConnectDB()
 	seeders.SeedRoles(config.DB)
 	seeders.SeedAdminUser(config.DB)
-	utils.InitGCS()
+
 
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000","https://si-baper.vercel.app"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://si-baper.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
@@ -36,7 +34,7 @@ func main() {
 	}))
 
 	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong! SI-BAPER API is running."})
+		c.JSON(200, gin.H{"message": "pong! SI-BAPER API is running smoothly."})
 	})
 
 	routes.SetupRoutes(router)
